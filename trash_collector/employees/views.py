1192d6fec7ea9b45a3185c8d804f55e21f480896
Dillon Coleman <coleman399@gmail.com>
2021-11-01 12:36:54 -0700

    added employee model, create html, and url. created database

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+from .models import Employee
 
 # Create your views here.
 
@@ -11,3 +17,15 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip')
+        new_customer = Employee(name=name_from_form, user=logged_in_user, zip_code=zip_from_form)
+        new_customer.save()
+        return HttpResponseRedirect(reverse('employee:index'))
+    else:
+        return render(request, 'employee/create.html')
